426c14e8c59aaade2552f8c9e7966e950bceb4e1
JoeMilius <joemilius@gmail.com>
2021-06-15 12:01:01 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array, name){
+    let noCaseDriver = array.filter(function(driver){return driver.toLowerCase() === name.toLowerCase()})
+    let noDrivers = []
+    if (noCaseDriver){
+        return noCaseDriver
+    }else if (array.filter(function(driver){return driver !== name})){
+        return noDrivers
+    }
+
+}
+
+function fuzzyMatch(array, name){
+    return array.filter(function(driver){return driver[0,1] === name[0,1]})
+}
+
+function matchName(array, person){
+     return (array.filter(function(driver){return driver.name === person}))
+
+
+}
